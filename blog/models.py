a2d72e341aa0b698b6ed48ba67e6151eaab9880a
Syed Muhammad Jafri <syedjafri@nyu.edu>
2018-08-16 00:43:41 -0400

    Added bootstrap and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    #author = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
